9591f3338886290920a8a6ecb1bf49d67d785ba9
John Major <daylily@daylilyinformatics.com>
2024-10-01 10:39:06 -0700

    X

--- run_slim_goodie_ui.sh
+++ run_slim_goodie_ui.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+echo "usage: IP:0.0.0.0 PORT:8918 
 # Check if $1 is null and set host accordingly
 if [ -z "$1" ]; then
   host="0.0.0.0"
+else
+  host="$1"
+fi
+
+
+# Check if $1 is null and set host accordingly
+if [ -z "$2" ]; then
+  port="8918"
 else
   host="$1"
 fi
@@ -21,13 +30,13 @@ fi
 num_workers=$(( (num_cores * 2) - 1 ))
 
 # Run Uvicorn for development or Gunicorn for production
-if [ -z "$2" ]; then
+if [ -z "$3" ]; then
   echo "Running in dev mode with 1 worker on $host"
   sleep 2
-  uvicorn main:app --reload --log-level trace --port 8918 --timeout-keep-alive 303 --host $host
+  uvicorn main:app --reload --log-level trace --port $PORT --timeout-keep-alive 303 --host $host
 else
   echo "RUNNING IN PRODUCTION MODE"
   echo "Running with $num_workers workers on $host"
   sleep 4
-  gunicorn main:app -w $num_workers -k uvicorn.workers.UvicornWorker --log-level trace --timeout 303 --bind $host:8918
+  gunicorn main:app -w $num_workers -k uvicorn.workers.UvicornWorker --log-level trace --timeout 303 --bind $host:$PORT
 fi
